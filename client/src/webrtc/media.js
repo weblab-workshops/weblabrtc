b34d0ee56dad2f34dad0db501ccf9d3f7baf4827
ShashvatS <shashvat.sri@gmail.com>
2023-01-20 16:34:43 -0500

    deleted files

--- client/src/webrtc/media.js
+++
@@ -1,9 +0,0 @@
-// const constraints = { video: true, audio: true };
-
-// let mediaStream = null;
-
-// navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-
-//   stream.for
-//   stream = null;
-// });
